5474913d197479b83b26750cd378d6734049fa78
Janine Santos <janinejnsantos70@gmail.com>
2025-03-18 21:16:02 -0300

    alteração do projeto após uma análise mais ampla do desafio

--- app.js
+++ app.js
@@ -1,26 +1,34 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 let amigos = [];
-let resultado = sortearAmigo();
 
 function nomeNaTela(tag, texto) {
   let titulos = document.querySelector(tag);
   titulos.innerHTML = texto;
 }
+
 function adicionarAmigo() {
   let campoSortear = document.querySelector("input").value;
-  console.log(campoSortear);
+  if (campoSortear.trim() !== "") {
+    amigos.push(campoSortear);
+    console.log(`Amigo adicionado: ${campoSortear}`);
+    atualizarLista();
+    alert("O campo está vazio. Por favor, preencha novamente:");
+  }
 }
 
-function atualizacaoLista() {
+function atualizarLista() {
   let listaAmigo = document.getElementById("listaAmigo");
-  listaAmigo.innerHTML = "";
+  listaAmigo.innerHTML = amigos.map((amigo) => `<li>${amigo}</li>`).join("");
 }
 
 function sortearAmigo() {
-  let nomeSorteado = amigos[Math.random() * amigos.length];
-  let resultado = document.getElementById("resultado");
-  resultado = `O seu amigo secreto é ${nomeSorteado}!`;
-  if (resultado == nomeSorteado) {
-    nomeNaTela("h2", "Você descobriu o nome sorteado!");
+  if (amigos.length === 0) {
+    nomeNaTela("h2", "Adicione amigos antes de sortear!");
+    return;
   }
+
+  let nomeSorteado = amigos[Math.floor(Math.random() * amigos.length)];
+  let resultado = document.getElementById("resultado");
+
+  resultado.innerHTML = `O seu amigo secreto é ${nomeSorteado}!`;
 }
